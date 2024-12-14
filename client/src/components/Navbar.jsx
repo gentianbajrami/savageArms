b729599779daded7f53e77f3f1593c264e839fc3
AltinBajrami <altin.bajrami66@gmail.com>
2024-12-14 15:47:22 +0100

    added navbar and landing page

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components';
+import {
+  FaSearch,
+  FaMagento,
+  FaBars,
+} from 'react-icons/fa';
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <div className="nav-center">
+        <p className="logo">G-Arms</p>
+        <div className="menu-btn">
+          <FaBars />
+        </div>
+
+        <div className="links">
+          <a href="#">Firearms</a>
+          <a href="#">Accessories</a>
+          <a href="#">Performance</a>
+          <a href="#">Blog</a>
+          <a href="#">
+            <FaMagento />
+          </a>
+          <a href="#">
+            <FaSearch />
+          </a>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.nav`
+  width: 100%;
+  background: rgba(1, 1, 1, 0.5);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  height: 5rem;
+  display: grid;
+  place-items: center;
+  .nav-center {
+    display: grid;
+    grid-template-columns: 200px 1fr;
+    justify-content: space-between;
+    align-items: center;
+    width: 90%;
+    max-width: 1240px;
+    margin: 0 auto;
+    .logo {
+      justify-self: start;
+      font-size: 3rem;
+      text-transform: uppercase;
+      font-weight: bold;
+      color: white;
+    }
+    .menu-btn {
+      display: none;
+    }
+    .links {
+      justify-self: end;
+      display: flex;
+      font-size: 1.1rem;
+      text-transform: uppercase;
+      margin-right: 2rem;
+
+      color: white;
+      a {
+        color: white;
+        text-decoration: none;
+        font-weight: 400;
+        padding: 0.5rem 1rem;
+        letter-spacing: 1px;
+        border-radius: 5px;
+      }
+      :hover {
+        background-color: antiquewhite;
+        color: black;
+      }
+    }
+    @media (max-width: 992px) {
+      .menu-btn {
+        display: block;
+        justify-self: end;
+        font-size: 2rem;
+      }
+      .links {
+        display: none;
+      }
+    }
+  }
+`;
+
+export default Navbar;
--- client/src/pages/MainLayout.jsx
+++ client/src/pages/MainLayout.jsx
@@ -1,6 +1,16 @@
+import styled from 'styled-components';
+import { Navbar } from '../components';
+import { Outlet } from 'react-router-dom';
+
 const MainLayout = () => {
   return (
-    <div>MainLayout Page</div>
-  )
-}
-export default MainLayout
+    <Wrapper>
+      <Navbar />
+      <Outlet />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div``;
+
+export default MainLayout;
