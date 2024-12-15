dddcbc62fbf8d6fe6f1076a5453fa3305c3fea58
Gentian <gentian.bajrami20@outlook.com>
2024-12-15 15:35:36 +0100

    added custom Errors, routes, middleware, controller, model folders

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log('server is working');
+import 'express-async-errors';
+import express from 'express';
+const app = express();
+import morgan from 'morgan';
+import * as dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const port = process.env.PORT || 5100;
+
+app.use(express.json());
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
