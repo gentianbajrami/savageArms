f52cb5f60a78e0ccd6fc534c129503e91f1c5431
AltinBajrami <altin.bajrami66@gmail.com>
2025-01-07 19:58:30 +0100

    Added sidebar and dashboard navbar

---
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Dashboard = () => {
+  return <Wrapper>Dashboard</Wrapper>;
+};
+
+const Wrapper = styled.aside`
+  background-color: blue;
+`;
+export default Dashboard;
