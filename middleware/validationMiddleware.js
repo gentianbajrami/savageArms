dabcfc8cbc33417d50a4db5886f53186fb4693ef
AltinBajrami <altin.bajrami66@gmail.com>
2025-01-13 14:52:08 +0100

    create crud for blog

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,14 +1,19 @@
-import { body, validationResult } from 'express-validator';
+import {
+  body,
+  validationResult,
+} from 'express-validator';
 import { BadRequestError } from '../errors/customErrors.js';
 import UserModel from '../models/UserModel.js';
 
-const withValidationErrors = (validateValues) => {
+const withValidationErrors = validateValues => {
   return [
     validateValues,
     (req, res, next) => {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map((error) => error.msg);
+        const errorMessages = errors
+          .array()
+          .map(error => error.msg);
         throw new BadRequestError(errorMessages);
       }
       next();
@@ -16,50 +21,87 @@ const withValidationErrors = (validateValues) => {
   ];
 };
 
-export const validateRegisterInput = withValidationErrors([
-  body('firstName').notEmpty().withMessage('First Name is required'),
-  body('lastName').notEmpty().withMessage('Last Name is required'),
-  body('address').notEmpty().withMessage('Address is required'),
-  body('email')
-    .notEmpty()
-    .withMessage('Email is required')
-    .isEmail()
-    .withMessage('invalid email format')
-    .custom(async (email) => {
-      const user = await UserModel.findOne({ email });
-      if (user) {
-        throw new BadRequestError('email already exists!');
-      }
-    }),
-  body('password')
-    .notEmpty()
-    .withMessage('Password is required')
-    .isLength({ min: 8 })
-    .withMessage('Password must be at least 8 characters long'),
-]);
+export const validateRegisterInput =
+  withValidationErrors([
+    body('firstName')
+      .notEmpty()
+      .withMessage('First Name is required'),
+    body('lastName')
+      .notEmpty()
+      .withMessage('Last Name is required'),
+    body('address')
+      .notEmpty()
+      .withMessage('Address is required'),
+    body('email')
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('invalid email format')
+      .custom(async email => {
+        const user = await UserModel.findOne({
+          email,
+        });
+        if (user) {
+          throw new BadRequestError(
+            'email already exists!'
+          );
+        }
+      }),
+    body('password')
+      .notEmpty()
+      .withMessage('Password is required')
+      .isLength({ min: 8 })
+      .withMessage(
+        'Password must be at least 8 characters long'
+      ),
+  ]);
 
-export const validateLoginInput = withValidationErrors([
-  body('email')
-    .notEmpty()
-    .withMessage('email is required')
-    .isEmail()
-    .withMessage('invalid email format'),
-    body('password').notEmpty().withMessage('password is required')
-]);
+export const validateLoginInput =
+  withValidationErrors([
+    body('email')
+      .notEmpty()
+      .withMessage('email is required')
+      .isEmail()
+      .withMessage('invalid email format'),
+    body('password')
+      .notEmpty()
+      .withMessage('password is required'),
+  ]);
 
-const validateUpdateUserInput = withValidationErrors([
-  body('firstName').notEmpty().withMessage('First Name is required'),
-  body('lastName').notEmpty().withMessage('Last Name is required'),
-  body('address').notEmpty().withMessage('Address is required'),
-  body('email')
-    .notEmpty()
-    .withMessage('Email is required')
-    .isEmail()
-    .withMessage('invalid email format')
-    .custom(async (email) => {
-      const user = await UserModel.findOne({ email });
-      if (user) {
-        throw new BadRequestError('email already exists!');
-      }
-    }),
-]);
+const validateUpdateUserInput =
+  withValidationErrors([
+    body('firstName')
+      .notEmpty()
+      .withMessage('First Name is required'),
+    body('lastName')
+      .notEmpty()
+      .withMessage('Last Name is required'),
+    body('address')
+      .notEmpty()
+      .withMessage('Address is required'),
+    body('email')
+      .notEmpty()
+      .withMessage('Email is required')
+      .isEmail()
+      .withMessage('invalid email format')
+      .custom(async email => {
+        const user = await UserModel.findOne({
+          email,
+        });
+        if (user) {
+          throw new BadRequestError(
+            'email already exists!'
+          );
+        }
+      }),
+  ]);
+
+export const validateCreateBlogInput =
+  withValidationErrors([
+    body('title')
+      .notEmpty()
+      .withMessage('Title is required'),
+    body('content')
+      .notEmpty()
+      .withMessage('Content is required'),
+  ]);
--- server.js
+++ server.js
@@ -6,14 +6,22 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
 
 //routers
 import authRouter from './routes/authRouter.js';
 import userRouter from './routes/userRouter.js';
+import blogRouter from './routes/blogRouter.js';
 
 //middlewares
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 import { authenticateUser } from './middleware/authMiddleware.js';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -22,8 +30,13 @@ if (process.env.NODE_ENV === 'development') {
 app.use(cookieParser());
 app.use(express.json());
 
-app.use('/api/v1/users', authenticateUser, userRouter);
+app.use(
+  '/api/v1/users',
+  authenticateUser,
+  userRouter
+);
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/blogs', blogRouter);
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
