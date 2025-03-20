81a3520a7bbbdbebbe7a8dcaaf9da0dbfc090cd1
AltinBajrami <altin.bajrami66@gmail.com>
2025-03-20 22:38:12 +0100

    added single product page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -23,3 +23,4 @@ export { default as BlogDashboard } from './Blog/BlogDashboard';
 export { default as EditBlog } from './Blog/EditBlog';
 export { default as Cart } from './Cart';
 export { default as Products } from './Products';
+export { default as SingleProductPage } from './SingleProductPage';
